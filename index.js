f6e78b82864f23fb1bf7b806fb441bc4dbffca7d
ssong <sing2song@naver.com>
2020-04-21 23:38:11 +0900

    Day02

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log('Im Working. Im JS. Im Beautiful. Imworth it');
+console.log("hello");
+console.log('hello');
+
+const array=[["1","A"],["2","B"],["3","C"]];//배열안에 배열
+console.log(array);
+
+const info={
+name:"ssong",
+like:["comic","movie"],//객체 안에 배열
+food:[
+    {
+     name:"choco",
+        sweet:true
+    },
+    {
+        name:"berry",
+        sweet:true//객체 안에 배열 안에 객체
+    }
+]
+};
+
+console.log(info);
+
+const object={
+name:"me",
+color:{
+    bright:true,
+    name:"pink"
+},
+age:55
+};//객체 안에 객체
+
+console.log(object);
