212a3e4e1adb199fae4d7a6dd6af6e2f09dd85b8
ssong <sing2song@naver.com>
2020-05-11 00:48:14 +0900

    weather.js 수정
    쪼개서 공부해라!

--- weather.js
+++ weather.js
@@ -1,6 +1,26 @@
-const API_KEY='41d3fd8e58c752ef27d86b4ab3b73d7d';
+const weather = document.querySelector(".js-weather");
 
+const API_KEY="41d3fd8e58c752ef27d86b4ab3b73d7d";
 const COORDS ='coords';
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //then()을 2번쓰는 이유 위의 json의 로딩 후 함수를 움직이게 할려고
+            //console.log(json);
+
+            const temparature = json.main.temp;
+            const place = json.name;
+
+            weather.innerText = `${temparature} @ ${place}`;
+        });
+}
+
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS,JSON.stringify(coordsObj));
@@ -16,6 +36,7 @@ function handleGeoSuccess(position){
    //객체의 변수 이름과 key의 이름을 같게 저장할 때 위와 같이 생략가능
 
    saveCoords(coordsObj);
+   getWeather(latitude,longitude);
 }
 
 function handleGeoError(){
@@ -34,7 +55,8 @@ function loadCoords(){
     if(loadedCoords==null){
         askForCoords();//좌표처리
     }else{
-
+        const parseCoords=JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
     }
 }
 
